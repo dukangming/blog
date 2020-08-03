f43891effae487e7782e57f3f65a398e435afaea
dkm <690643536@qq.com>
2020-08-03 17:43:01 +0800

    增加login页面

---
+++ admin/src/router.js
@@ -0,0 +1,21 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./views/login.vue"
+
+Vue.use(Router);
+
+export default new Router({
+    mode: "history",
+    //内置变量
+    base: process.env.BASE_URL,
+    routes: [{
+        path: "*", //随意输入什么
+        redirect: "/login", //跳转到"/login"
+    }, {
+        path: "",
+        redirect: "/login",
+    }, {
+        path: "/login",
+        component: Login //"/login"对应Login组件
+    }]
+})
