34b40d54ef089b92e9697501fe819cd34860b9cd
dkm <690643536@qq.com>
2020-08-04 10:16:15 +0800

    增加大章

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,1 +1,7 @@
-INSERT INTO test (id, name) VALUES (1212,"sadadswqasd")
+drop table if exists `chapter`;
+create table `chapter` (
+  `id` char(8) not null comment 'id',
+  `course_id` char(8) comment '课程id',
+  `name` varchar(50) comment '名称',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
