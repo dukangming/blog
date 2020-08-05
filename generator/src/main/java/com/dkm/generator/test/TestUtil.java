e3ff11851251c62c6b10b6a5a3728d4b017820e9
dkm <690643536@qq.com>
2020-08-05 10:56:34 +0800

    generator

---
+++ generator/src/main/java/com/dkm/generator/test/TestUtil.java
@@ -0,0 +1,34 @@
+package com.dkm.generator.test;
+
+/**
+ * @author dkm
+ * @create 2020-08-05 10:53
+ */
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestUtil {
+
+    static String ftlPath = "generator\\src\\main\\java\\com\\dkm\\generator\\test\\";
+    static String toPath = "generator\\src\\main\\java\\com\\dkm\\generator\\test\\";
+
+    public static void main(String[] args) throws IOException, TemplateException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File(ftlPath));
+        cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
+        Template temp = cfg.getTemplate("test.ftl");
+
+        FileWriter fw = new FileWriter(toPath + "Test.java");
+        BufferedWriter bw = new BufferedWriter(fw);
+        temp.process(null, bw);
+        bw.flush();
+        fw.close();
+    }
+}
---
+++ generator/src/main/java/com/dkm/generator/test/test.ftl
@@ -0,0 +1,8 @@
+package com.dkm.generator.test;
+
+public class Test {
+    /**
+    * ID
+    */
+    private String id;
+}
