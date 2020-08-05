ae0d135f2d633e3cfb897193878e3731eb8f09dd
dkm <690643536@qq.com>
2020-08-05 15:34:40 +0800

    生成service，controller

---
+++ business/src/main/java/com/dkm/business/controller/admin/SectionController.java
@@ -0,0 +1,66 @@
+package com.dkm.business.controller.admin;
+
+import com.dkm.server.dto.SectionDto;
+import com.dkm.server.dto.PageDto;
+import com.dkm.server.dto.ResponseDto;
+import com.dkm.server.service.SectionService;
+import com.dkm.server.util.UuidUtil;
+import com.dkm.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author dkm
+ * @create 2020-08-03 9:54
+ */
+
+@RestController
+@RequestMapping("/admin/section")
+public class SectionController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SectionController.class);
+    public static final String BUSINESS_NAME = "小节";
+
+    @Autowired
+    public SectionService sectionService;
+    //@RequestBody接收json 默认接收formdata
+
+    /**
+     * 列表查询
+     */
+    @PostMapping(value="/list",produces= {"application/json;charset=UTF-8"})
+    public ResponseDto section(@RequestBody PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
+        sectionService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @PostMapping(value="/save",produces= {"application/json;charset=UTF-8"})
+    public ResponseDto save(@RequestBody SectionDto sectionDto) {
+
+        // 保存校验
+
+
+        ResponseDto responseDto = new ResponseDto();
+        sectionService.save(sectionDto);
+        responseDto.setContent(sectionDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除
+     */
+    @DeleteMapping(value="/delete/{id}",produces= {"application/json;charset=UTF-8"})
+    public ResponseDto delete(@PathVariable String id) {
+        LOG.info("id:{}",id);
+        ResponseDto responseDto = new ResponseDto();
+        sectionService.delete(id);
+        return responseDto;
+    }
+}
